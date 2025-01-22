2cdb9782f30ba227b1734ef76b7582cb7e196c3b
Rawan-Temo <rinas1122k@gmail.com>
2025-01-22 09:29:48 +0300

    users

--- controllers/login/userController.js
+++ controllers/login/userController.js
@@ -40,7 +40,7 @@ const allUsers = async (req, res) => {
 };
 const userById = async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
+    const user = await User.findById(req.params.id).populate("sectionId");
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
